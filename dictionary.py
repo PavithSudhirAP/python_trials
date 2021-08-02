15b840a967542ed467a4bc6d9c540d7d059227b8
Pavith Sudhir <56886753+PavithSudhirAP@users.noreply.github.com>
2021-08-02 11:02:26 +0000

    dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+import json
+from difflib import get_close_matches as match
+
+data = json.load(open("dictionary.json"))
+keys = data.keys()
+
+def meaning(word):
+  word = word.lower()
+  if word in data:
+    return data[word]
+  else:
+    matched = match(word,keys)[0]
+    print("Did you mean {} instead of {} ?".format(matched,word))
+    return "Try again!!"
+
+word = input("Enter word= ")
+
+print(meaning(word))
